4086f482cd79598c3d99b1c4064116975507ffb6
Katya Weicht <katya.weicht@gmail.com>
2025-08-05 10:37:34 -0600

    pass data to ArtList through outlet in Gallery component

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import NavBar from "../components/NavBar"
 import { Outlet } from "react-router-dom"
+import useArt from "../hooks/useArt"
 
 function Gallery() {
+  const {catArt} = useArt()
   return (
     <>
     <NavBar />
     <p>Gallery of Cat Art from API</p>
-    <Outlet />
+    <Outlet context = {{catArt}}/>
     </>
   )
 }
