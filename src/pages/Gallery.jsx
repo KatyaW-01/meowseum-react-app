4899f75c47fb902703a710671cdc2852ff362d6f
Katya Weicht <katya.weicht@gmail.com>
2025-08-06 17:32:11 -0600

    Update label and options

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -6,11 +6,22 @@ import useArt from "../hooks/useArt"
 function Gallery() {
   const {catArt} = useArt()
   return (
-    <>
+    <div>
     <NavBar />
     <h1>Gallery </h1>
+    <form>
+      <label for="artType">Filter Art:</label>
+      <select name="artType" id="artType" defaultValue="">
+        <option value="" disabled >Select type</option>
+        <option value="sculpture">Sculpture</option>
+        <option value="print">Print</option>
+        <option value="textile">Textile</option>
+        <option value="ceramics">Ceramics</option>
+        <option value="photograph">Photograph</option>
+      </select>
+    </form>
     <Outlet context = {{catArt}}/>
-    </>
+    </div>
   )
 }
 
