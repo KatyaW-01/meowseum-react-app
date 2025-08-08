5a279de99fd22bd7026d432596c60276e1379e3e
Katya Weicht <katya.weicht@gmail.com>
2025-08-07 21:54:55 -0600

    Move fetch call for favorites to hook

--- src/hooks/useArt.js
+++ src/hooks/useArt.js
@@ -28,7 +28,6 @@ function useArt() {
             const data = await response.json()
             const iiifUrl = data.config.iiif_url
             const imageID = data.data.image_id
-            console.log("art id:",data.data.id)
             const artDetails = {
               'id': data.data.id,
               'title': data.data.title,
@@ -51,7 +50,35 @@ function useArt() {
     })
   },[catArt])
 
-  return {catArtDetails}
+  const storedIds = JSON.parse(localStorage.getItem("artID"))
+  
+  const [artData, setArtData] = useState([])
+  
+  useEffect(()=> {
+    if (storedIds !== null) {
+      storedIds.map((id)=> {
+        fetch(`https://api.artic.edu/api/v1/artworks/${id}`)
+        .then((response) => response.json())
+        .then((data)=> {
+          const iiifUrl = data.config.iiif_url
+          const imageID = data.data.image_id
+          const art_data = {
+            'id': id,
+            'fullImageUrl': `${iiifUrl}/${imageID}/full/843,/0/default.jpg`,
+            'title': data.data.title,
+            'artist': data.data.artist_display
+          }
+          setArtData(prev => {
+            const alreadyExists = prev.some(art => art.id === art_data.id)
+            return alreadyExists ? prev : [...prev, art_data]
+          })
+
+        })    
+      })
+    }
+  },[])
+
+  return {catArtDetails, artData, setArtData}
 
 }
 
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,42 +2,15 @@ import React from "react"
 import NavBar from "../components/NavBar"
 import { Outlet } from "react-router-dom"
 import { useEffect, useState } from "react"
+import useArt from "../hooks/useArt"
 
 function Favorites() {
-  const storedIds = JSON.parse(localStorage.getItem("artID"))
-
-  const [artData, setArtData] = useState([])
-
-  useEffect(()=> {
-    if (storedIds !== null) {
-      storedIds.map((id)=> {
-        fetch(`https://api.artic.edu/api/v1/artworks/${id}`)
-        .then((response) => response.json())
-        .then((data)=> {
-          const iiifUrl = data.config.iiif_url
-          const imageID = data.data.image_id
-          const art_data = {
-            'id': id,
-            'fullImageUrl': `${iiifUrl}/${imageID}/full/843,/0/default.jpg`,
-            'title': data.data.title,
-            'artist': data.data.artist_display
-          }
-          setArtData(prev => {
-            const alreadyExists = prev.some(art => art.id === art_data.id)
-            return alreadyExists ? prev : [...prev, art_data]
-          })
-
-        })    
-      })
-    }
-  },[])
+  const {artData, setArtData} = useArt()
 
   function handleClear() {
     localStorage.removeItem("artID")
     setArtData([])
   }
-
-  console.log(storedIds)
 
   return (
     <>
